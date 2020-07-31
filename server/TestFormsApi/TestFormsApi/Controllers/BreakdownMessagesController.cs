c5e69af7b1a83e43d7a11afac0b45eaa495d38fd
Игорь Мокроусов <mokrousov.im@students.dvfu.ru>
2020-07-31 15:32:33 +1000

    Init

---
+++ server/TestFormsApi/TestFormsApi/Controllers/BreakdownMessagesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestFormsApi.Entities;
+using Newtonsoft.Json;
+
+namespace TestFormsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BreakdownMessagesController : ControllerBase
+    {
+        private readonly ILogger<BreakdownMessagesController> _logger;
+
+        public BreakdownMessagesController(ILogger<BreakdownMessagesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<object>> GetBreakdownMessanges([FromQuery] string text)
+        {
+            var result = new List<object>();
+            foreach(var message in BreakdownMessages.Messages)
+            {
+                foreach(var keyValuePair in JsonConvert.DeserializeObject<Dictionary<string, string>>(message.ToString()))
+                {
+                    if (keyValuePair.Value.Contains(text))
+                    {
+                        result.Add(message);
+                        break;
+                    }
+                }
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200)]
+        public ActionResult SaveBreakdownMessage([FromBody] object breakdownMessage)
+        {
+            BreakdownMessages.Messages.Add(breakdownMessage);
+            
+            return Ok();
+        }
+    }
+}
---
+++ server/TestFormsApi/TestFormsApi/Entities/BreakdownMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestFormsApi.Entities
+{
+    public static class BreakdownMessages
+    {
+        public static List<object> Messages = new List<object>();
+    }
+}
